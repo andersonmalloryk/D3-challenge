3e4fc6be09870fe32a5effb0b3ffdfb110b6174d
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-04-21 20:36:50 -0500

    added instructions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 console.log("reading app.js")
 
+// create scatter plot between two data variables
+// income v obesity 
+
+// scatter plot represents each state with circle elements
+
+// add state abreviations in circles .abbr
+
+// add hover text that includes the data
 
